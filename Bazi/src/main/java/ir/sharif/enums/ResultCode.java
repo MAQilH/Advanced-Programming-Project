13842cad88c4e6b03cf434764f4c1dac5fb68d75
KianWM <kiankhadem@icloud.com>
2024-06-23 16:10:37 +0330

    Command Result Ok shod

--- Bazi/src/main/java/ir/sharif/enums/ResultCode.java
+++ Bazi/src/main/java/ir/sharif/enums/ResultCode.java
@@ -1,2 +1,18 @@
-package ir.sharif.enums;public enum ResultCode {
+package ir.sharif.enums;
+
+public enum ResultCode {
+
+    NotFound(404),
+    InvalidCommand(100);
+
+    private final int result;
+
+    ResultCode(int code) {
+        this.result = code;
+    }
+
+    public int getResultCode() {
+        return this.result;
+    }
+
 }
