f42794bb0508e1fc17bc5add7b3a10440ab8575b
KianWM <kiankhadem@icloud.com>
2024-06-25 16:15:05 +0330

    nothing

--- Bazi/src/main/java/ir/sharif/model/game/Leader.java
+++ Bazi/src/main/java/ir/sharif/model/game/Leader.java
@@ -4,10 +4,28 @@ public class Leader {
     private final String name;
     private final Faction faction;
     private final Ability ability;
+    private boolean isUsed = false;
 
     public Leader(String name, Faction faction, Ability ability) {
         this.name = name;
         this.faction = faction;
         this.ability = ability;
     }
+
+    public String getName() {
+        return name;
+    }
+    public Faction getFaction() {
+        return faction;
+    }
+    public Ability getAbility() {
+        return ability;
+    }
+    public boolean isUsed() {
+        return isUsed;
+    }
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
+
 }
