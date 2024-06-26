4b492d3643955dcde5756008968aebedc94f5c90
KianWM <kiankhadem@icloud.com>
2024-06-26 15:52:34 +0330

    everything that Sohsoh said is fixed now

--- Bazi/src/main/java/ir/sharif/model/game/Card.java
+++ Bazi/src/main/java/ir/sharif/model/game/Card.java
@@ -1,5 +1,9 @@
 package ir.sharif.model.game;
 
+import ir.sharif.controller.GameController;
+import ir.sharif.service.AppService;
+import ir.sharif.service.GameService;
+
 public class Card {
     private final String name;
     private final int power, noOfCards;
@@ -48,6 +52,41 @@ public class Card {
 
 	public CardPosition getCardPosition() {
 		return cardType;
+	}
+
+	public int findCardsPosition() {
+		MatchTable matchTable = GameService.getInstance().getMatchTable();
+		if(matchTable.getUserTable(1).getRowByNumber(2).getCards().contains(this)) {
+			return 0;
+		} if(matchTable.getUserTable(1).getRowByNumber(1).getCards().contains(this)) {
+			return 1;
+		} if(matchTable.getUserTable(1).getRowByNumber(0).getCards().contains(this)) {
+			return 2;
+		} if(matchTable.getUserTable(0).getRowByNumber(0).getCards().contains(this)) {
+			return 3;
+		} if(matchTable.getUserTable(0).getRowByNumber(1).getCards().contains(this)) {
+			return 4;
+		} if(matchTable.getUserTable(0).getRowByNumber(2).getCards().contains(this)) {
+			return 5;
+		} if(matchTable.getUserTable(1).getRowByNumber(2).getSpell() == this) {
+			return 6;
+		} if(matchTable.getUserTable(1).getRowByNumber(1).getSpell() == this) {
+			return 7;
+		} if(matchTable.getUserTable(1).getRowByNumber(0).getSpell() == this) {
+			return 8;
+		} if(matchTable.getUserTable(0).getRowByNumber(0).getSpell() == this) {
+			return 9;
+		} if(matchTable.getUserTable(0).getRowByNumber(1).getSpell() == this) {
+			return 10;
+		} if(matchTable.getUserTable(0).getRowByNumber(2).getSpell() == this) {
+			return 11;
+		} return -1;
+	}
+
+	public int calculatePower() {
+		int pos = findCardsPosition();
+		GameController gameController = GameService.getInstance().getController();
+		return gameController.calculatePower(pos, this);
 	}
 
 }
