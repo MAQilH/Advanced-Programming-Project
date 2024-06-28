4a99ba06f17e09b7efbd7d305d2926289a0cacb9
Soheil Mohammadkhani <mohammadkhani.soheil@gmail.com>
2024-06-28 12:40:38 +0330

    yayy

---
+++ Bazi/src/main/java/ir/sharif/messages/Friends/PendingFriendRequests.java
@@ -0,0 +1,12 @@
+package ir.sharif.messages.Friends;
+
+import ir.sharif.messages.ClientMessage;
+import ir.sharif.messages.ClientMessageType;
+
+public class PendingFriendRequests extends ClientMessage {
+	private String username;
+	PendingFriendRequests(String username) {
+		this.username = username;
+		this.type = ClientMessageType.PENDING_FRIEND_REQUESTS;
+	}
+}
