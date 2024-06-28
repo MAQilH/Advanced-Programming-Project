16820861abf36b650d5c6f54406dd8107a55a97f
Soheil Mohammadkhani <mohammadkhani.soheil@gmail.com>
2024-06-28 18:14:44 +0330

    fuck

---
+++ Bazi/src/main/java/ir/sharif/messages/react/ReactMessage.java
@@ -0,0 +1,13 @@
+package ir.sharif.messages.react;
+
+import ir.sharif.messages.ClientMessage;
+import ir.sharif.messages.ClientMessageType;
+import ir.sharif.model.React;
+
+public class ReactMessage extends ClientMessage {
+	private React react;
+	public ReactMessage(String sender, String message) {
+		this.react = new React(sender, message);
+		this.type = ClientMessageType.REACT_MESSAGE;
+	}
+}
