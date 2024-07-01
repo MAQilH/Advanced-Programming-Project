d85541dde2e1ce01b26e3f1248507565d47ddd04
Soheil Mohammadkhani <mohammadkhani.soheil@gmail.com>
2024-07-01 13:41:29 +0330

    fuck

--- Bazi/src/main/java/ir/sharif/view/GUIApplication.java
+++ Bazi/src/main/java/ir/sharif/view/GUIApplication.java
@@ -143,9 +143,9 @@ public class GUIApplication extends Application {
     }
 
 	public static void main(String[] args) {
-		//username = args[0];
-	   String felan = TwoFactorAuth.getInstance().sendAuthCode("sohsoh84@gmail.com");
-		System.err.println(felan);
-		//launch();
+		username = args[0];
+	  // String felan = TwoFactorAuth.getInstance().sendAuthCode("sohsoh84@gmail.com");
+	//	System.err.println(felan);
+		launch();
 	}
 }
