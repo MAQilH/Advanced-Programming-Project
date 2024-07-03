67be7f8950ce2fac9c29d592837f0d7af80ee5f8
KianWM <kiankhadem@icloud.com>
2024-07-03 21:06:41 +0330

    null cards bug have been fixed

--- Bazi/src/main/java/ir/sharif/model/game/abilities/HisImperialMajesty.java
+++ Bazi/src/main/java/ir/sharif/model/game/abilities/HisImperialMajesty.java
@@ -2,13 +2,28 @@ package ir.sharif.model.game.abilities;
 
 import ir.sharif.model.game.Ability;
 import ir.sharif.model.game.Card;
+import ir.sharif.model.game.MatchTable;
 import ir.sharif.model.game.Row;
+import ir.sharif.service.GameService;
+import ir.sharif.utils.Random;
+
+import java.util.ArrayList;
 
 public class HisImperialMajesty implements Ability {
 
     @Override
     public void execute(Object... objs) {
-        // TODO: last
+        MatchTable matchTable = GameService.getInstance().getMatchTable();
+        ArrayList<Card> toBeShown = new ArrayList<>();
+        ArrayList<Card> cardsInHand = new ArrayList<>();
+        cardsInHand.addAll(matchTable.getUserTable(matchTable.getTurn()).getHand());
+        for(int i = 0; i < 3; i++) {
+            if(cardsInHand.isEmpty()) break;
+            Card card = Random.getRandFromArrayListCard(cardsInHand);
+            cardsInHand.remove(card);
+            toBeShown.add(card);
+        }
+        //TODO: call the graphic method to show this ArrayList
     }
 
 }
